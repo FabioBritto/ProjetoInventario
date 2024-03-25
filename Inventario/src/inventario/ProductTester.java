1866eba1d769614aba0b43a2086a3db019d026f1
FabioBritto <fabio.tritono@gmail.com>
2024-03-25 16:26:51 -0300

    Feito o mesmo para o PRODUTO 2. Adicionar "sc.nextLine();" como forma de esvaziar o Buffer.

--- Inventario/src/inventario/ProductTester.java
+++ Inventario/src/inventario/ProductTester.java
@@ -25,12 +25,23 @@ public class ProductTester {
 		
 		Produto prod1 = new Produto(tempName,tempNumber,tempQuantidade,tempPreco);
 		
+		/*
+		 * Apesar de no meu caso, eu não ter passado pelo mesmo erro que o projeto preveu,
+		 * eu colocarei o "in.nextLine();" como forma de LIMPAR O BUFFER
+		 */
 		
-		Produto prod2 = new Produto();
-		prod2.setNomeProduto("Feijão");
-		prod2.setIdProduto(102);
-		prod2.setQtdeProduto(58);
-		prod2.setPrecoProduto(7.85);
+		sc.nextLine();
+		System.out.println("Por favor, digite o nome do Produto:");
+		tempName = sc.next();
+		System.out.println("Agora, por favor, insira o código do Produto:");
+		tempNumber = sc.nextInt();
+		System.out.println("Também precisamos que forneça a quantidade de Produtos disponíveis:");
+		tempQuantidade = sc.nextInt();
+		System.out.println("Por último, por favor, declare o preço do Produto:");
+		tempPreco = sc.nextDouble();
+		
+		Produto prod2 = new Produto(tempName,tempNumber,tempQuantidade,tempPreco);
+		
 		Produto prod3 = new Produto("Sal",103,32,7.85);
 		Produto prod4 = new Produto("Acúcar",104,23,10.35);
 		Produto prod5 = new Produto("Refrigerante",105,83,7.99);
