3c30d0df1df352f1747d0843237dd98f1bd41d4f
FabioBritto <fabio.tritono@gmail.com>
2024-03-25 16:13:27 -0300

    Criadas variáveis locais na Classe do Driver para receber os valores do usuário que serão usados como parâmetro na instanciação do Objeto.

--- Inventario/src/inventario/ProductTester.java
+++ Inventario/src/inventario/ProductTester.java
@@ -1,15 +1,32 @@
 package inventario;
 
+import java.util.Scanner;
+
 public class ProductTester {
 
 	public static void main(String[]args) {
 		
-		Produto prod1 = new Produto();
+		Scanner sc = new Scanner(System.in);
+		
+		String tempName;
+		int tempNumber;
+		int tempQuantidade;
+		double tempPreco;
+		
+		
+		System.out.println("Por favor, digite o nome do Produto:");
+		tempName = sc.next();
+		System.out.println("Agora, por favor, insira o código do Produto:");
+		tempNumber = sc.nextInt();
+		System.out.println("Também precisamos que forneça a quantidade de Produtos disponíveis:");
+		tempQuantidade = sc.nextInt();
+		System.out.println("Por último, por favor, declare o preço do Produto:");
+		tempPreco = sc.nextDouble();
+		
+		Produto prod1 = new Produto(tempName,tempNumber,tempQuantidade,tempPreco);
+		
+		
 		Produto prod2 = new Produto();
-		prod1.setNomeProduto("Arroz");
-		prod1.setIdProduto(101);
-		prod1.setQtdeProduto(43);
-		prod1.setPrecoProduto(19.90);
 		prod2.setNomeProduto("Feijão");
 		prod2.setIdProduto(102);
 		prod2.setQtdeProduto(58);
@@ -26,5 +43,6 @@ public class ProductTester {
 		System.out.println("\n"+ prod5.toString());
 		System.out.println("\n"+ prod6.toString());
 		
+		sc.close();
 	}
 }
