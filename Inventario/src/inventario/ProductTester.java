0d08360069e49c47f331d015872bbd678a7f9ae2
fabio.tritono@gmail.com <fabio.tritono@gmail.com>
2024-04-18 21:32:12 -0300

    V0.3
    Criados dois métodos. addToInventory() (static) e getNumProducts() que
    não era pra ser estático, mas o JAVA mudou e foi a única forma de fazer
    dar certo

--- Inventario/src/inventario/ProductTester.java
+++ Inventario/src/inventario/ProductTester.java
@@ -9,13 +9,54 @@ public class ProductTester {
 		Scanner sc = new Scanner(System.in);
 
 		Produto produtos[];
-		int maxSize = -1;
+		int maxSize;
+		
+
+		
+		maxSize = getNumProducts(sc);
+		//Posso construir o ARRAY fora do if ou dentro
+		produtos = new Produto[maxSize];
+		if(maxSize != 0) {
+			
+			addToInventory(produtos,sc);
+
+		}
+		else {
+			System.out.println("Não há produtos!");
+		}
+		displayInventory(produtos);
+		sc.close();
+	}
+	//Era pra retornar valor, mas aqui no caso já é um for each que imprime os produtos
+	public static void displayInventory(Produto[] produto) {
+		for(Produto prod : produto) {
+			System.out.println(prod);
+		}
+	}
+	public static void addToInventory(Produto[] produto,Scanner sc) {
 		String tempName;
 		int tempNumber;
 		int tempQuantidade;
 		double tempPreco;
 		boolean ativo;
-
+		for(int i = 0;i< produto.length;i++) {
+			sc.nextLine();
+			System.out.println("Por favor, digite o nome do Produto:");
+			tempName = sc.nextLine();
+			//sc.nextLine();
+			System.out.println("Agora, por favor, insira o código do Produto:");
+			tempNumber = sc.nextInt();
+			System.out.println("Também precisamos que forneça a quantidade de Produtos disponíveis:");
+			tempQuantidade = sc.nextInt();
+			System.out.println("Por último, por favor, declare o preço do Produto:");
+			tempPreco = sc.nextDouble();
+			Produto prod = new Produto(tempName,tempNumber,tempQuantidade,tempPreco,true);
+			produto[i] = prod;
+		}
+	}
+	//O método ficou estático por ORDENS DO ECLIPSE
+	public static int getNumProducts(Scanner sc) {
+		int maxSize = -1;
 		do {
 			try {
 				System.out.println("Por favor, insira uma quantidade positiva de produtos que deseja adicionar:\n"
@@ -38,38 +79,6 @@ public class ProductTester {
 				System.out.println("Tipo incorreto de dados inseridos!");
 			}
 		} while (maxSize < 0);
-		
-		//Posso construir o ARRAY fora do if ou dentro
-		produtos = new Produto[maxSize];
-		if(maxSize != 0) {
-			//produtos = new Produto[maxSize];
-			
-			for(int i = 0;i< produtos.length;i++) {
-				sc.nextLine();
-				System.out.println("Por favor, digite o nome do Produto:");
-				tempName = sc.nextLine();
-				//sc.nextLine();
-				System.out.println("Agora, por favor, insira o código do Produto:");
-				tempNumber = sc.nextInt();
-				System.out.println("Também precisamos que forneça a quantidade de Produtos disponíveis:");
-				tempQuantidade = sc.nextInt();
-				System.out.println("Por último, por favor, declare o preço do Produto:");
-				tempPreco = sc.nextDouble();
-				Produto prod = new Produto(tempName,tempNumber,tempQuantidade,tempPreco,true);
-				produtos[i] = prod;
-			}
-
-		}
-		else {
-			System.out.println("Não há produtos!");
-		}
-		displayInventory(produtos);
-		sc.close();
-	}
-	public static void displayInventory(Produto[] produto) {
-		for(Produto prod : produto) {
-			System.out.println(prod);
-		}
-		
+		return maxSize;
 	}
 }
